74c7a692ab9f46a1e53263447a0c97bd68db1c88
Eric Dumazet <eric.dumazet@gmail.com>
2011-05-23 23:07:32 +0000

    net: use synchronize_rcu_expedited()
    synchronize_rcu() is very slow in various situations (HZ=100,
    CONFIG_NO_HZ=y, CONFIG_PREEMPT=n)
    
    Extract from my (mostly idle) 8 core machine :
    
     synchronize_rcu() in 99985 us
     synchronize_rcu() in 79982 us
     synchronize_rcu() in 87612 us
     synchronize_rcu() in 79827 us
     synchronize_rcu() in 109860 us
     synchronize_rcu() in 98039 us
     synchronize_rcu() in 89841 us
     synchronize_rcu() in 79842 us
     synchronize_rcu() in 80151 us
     synchronize_rcu() in 119833 us
     synchronize_rcu() in 99858 us
     synchronize_rcu() in 73999 us
     synchronize_rcu() in 79855 us
     synchronize_rcu() in 79853 us
    
    When we hold RTNL mutex, we would like to spend some cpu cycles but not
    block too long other processes waiting for this mutex.
    
    We also want to setup/dismantle network features as fast as possible at
    boot/shutdown time.
    
    This patch makes synchronize_net() call the expedited version if RTNL is
    locked.
    
    synchronize_rcu_expedited() typical delay is about 20 us on my machine.
    
     synchronize_rcu_expedited() in 18 us
     synchronize_rcu_expedited() in 18 us
     synchronize_rcu_expedited() in 18 us
     synchronize_rcu_expedited() in 18 us
     synchronize_rcu_expedited() in 20 us
     synchronize_rcu_expedited() in 16 us
     synchronize_rcu_expedited() in 20 us
     synchronize_rcu_expedited() in 18 us
     synchronize_rcu_expedited() in 18 us
    
    Signed-off-by: Eric Dumazet <eric.dumazet@gmail.com>
    CC: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    CC: Ben Greear <greearb@candelatech.com>
    Reviewed-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Angelo G. Del Regno <kholk11@gmail.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -5985,7 +5985,10 @@ EXPORT_SYMBOL(free_netdev);
 void synchronize_net(void)
 {
 	might_sleep();
-	synchronize_rcu();
+	if (rtnl_is_locked())
+		synchronize_rcu_expedited();
+	else
+		synchronize_rcu();
 }
 EXPORT_SYMBOL(synchronize_net);
 
