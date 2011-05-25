57ae8630327b96b150a62785b54623648a649f63
Daniel Kiper <dkiper@net-space.pl>
2011-05-24 17:12:33 -0700

    mm: pfn_to_section_nr()/section_nr_to_pfn() is valid only in CONFIG_SPARSEMEM context
    pfn_to_section_nr()/section_nr_to_pfn() is valid only in CONFIG_SPARSEMEM
    context.  Move it to proper place.
    
    Signed-off-by: Daniel Kiper <dkiper@net-space.pl>
    Cc: Dave Hansen <dave@linux.vnet.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Angelo G. Del Regno <kholk11@gmail.com>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -935,9 +935,6 @@ static inline unsigned long early_pfn_to_nid(unsigned long pfn)
 #define pfn_to_nid(pfn)		(0)
 #endif
 
-#define pfn_to_section_nr(pfn) ((pfn) >> PFN_SECTION_SHIFT)
-#define section_nr_to_pfn(sec) ((sec) << PFN_SECTION_SHIFT)
-
 #ifdef CONFIG_SPARSEMEM
 
 /*
@@ -962,6 +959,9 @@ static inline unsigned long early_pfn_to_nid(unsigned long pfn)
 #if (MAX_ORDER - 1 + PAGE_SHIFT) > SECTION_SIZE_BITS
 #error Allocator MAX_ORDER exceeds SECTION_SIZE
 #endif
+
+#define pfn_to_section_nr(pfn) ((pfn) >> PFN_SECTION_SHIFT)
+#define section_nr_to_pfn(sec) ((sec) << PFN_SECTION_SHIFT)
 
 struct page;
 struct page_cgroup;
