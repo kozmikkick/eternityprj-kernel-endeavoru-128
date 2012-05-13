4cd0329a9febe065dfd1f9eddfd75d0f1e679986
Angelo G. Del Regno <kholk11@gmail.com>
2012-05-13 23:58:02 +0200

    A message to franciscofranco and the other ones.

---
+++ arch/arm/mach-tegra/EternityProject.c
@@ -0,0 +1,40 @@
+#include "thanks.h"
+
+//
+// EternityProject Public message START:
+//
+
+- To the "developers" like franciscofranco:
+Are you able to work on your own?
+Also, if you want to badly copy my commits, can you at least give credits
+to the proprietary of the commit you're copying?
+
+We're an opensource community, we do this for free... but we also are
+satisfacted of the TIME WE LOSE on the things we do.
+We want to work with everyone that wants to.
+We publish our sources.
+We give you all everything we do.
+
+
+And you?
+
+
+
+
+Instead of copying someone else's work, try to lose time on your own
+at least sorting the not working commits (yeah, I knew someone was
+copying my work and I've committed some fakes).
+
+
+You did that badly.
+
+
+
+
+The EternityProject Team Manager & Main Developer,
+--kholk
+
+
+//
+// EternityProject Public message END
+//
