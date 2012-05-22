c9092e47004e8ade19f60126a82c60d5ec5312be
Angelo G. Del Regno <kholk11@gmail.com>
2012-05-22 09:57:14 +0200

    ext4: reserve inodes and feature code for 'quota' feature

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -210,6 +210,8 @@ struct ext4_io_submit {
  */
 #define	EXT4_BAD_INO		 1	/* Bad blocks inode */
 #define EXT4_ROOT_INO		 2	/* Root inode */
+#define EXT4_USR_QUOTA_INO	 3	/* User quota inode */
+#define EXT4_GRP_QUOTA_INO	 4	/* Group quota inode */
 #define EXT4_BOOT_LOADER_INO	 5	/* Boot loader inode */
 #define EXT4_UNDEL_DIR_INO	 6	/* Undelete directory inode */
 #define EXT4_RESIZE_INO		 7	/* Reserved group descriptors inode */
