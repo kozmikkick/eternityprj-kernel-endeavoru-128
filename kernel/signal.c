dbbe15954b81f151a2330e0310522e6e290f4e29
Angelo G. Del Regno <kholk11@gmail.com>
2012-05-24 21:37:46 +0200

    ARM: use set_current_blocked() and block_sigmask()

--- kernel/signal.c
+++ kernel/signal.c
@@ -2119,6 +2119,27 @@ long do_no_restart_syscall(struct restart_block *param)
  * used by various programs)
  */
 
+/**
+ * block_sigmask - add @ka's signal mask to current->blocked
+ * @ka: action for @signr
+ * @signr: signal that has been successfully delivered
+ *
+ * This function should be called when a signal has succesfully been
+ * delivered. It adds the mask of signals for @ka to current->blocked
+ * so that they are blocked during the execution of the signal
+ * handler. In addition, @signr will be blocked unless %SA_NODEFER is
+ * set in @ka->sa.sa_flags.
+ */
+void block_sigmask(struct k_sigaction *ka, int signr)
+{
+	sigset_t blocked;
+
+	sigorsets(&blocked, &current->blocked, &ka->sa.sa_mask);
+	if (!(ka->sa.sa_flags & SA_NODEFER))
+		sigaddset(&blocked, signr);
+	set_current_blocked(&blocked);
+}
+
 /*
  * This is also useful for kernel threads that want to temporarily
  * (or permanently) block certain signals.
