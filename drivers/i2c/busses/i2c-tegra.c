72583480a8dcae986546cc20270b163ff762fd32
Angelo G. Del Regno <kholk11@gmail.com>
2012-05-30 22:10:38 +0200

    Revert "tegra: i2c: Added high speed support"
    This reverts commit cc2c556bb874ddf8e2475a40e981245cf4ddab4e.

--- drivers/i2c/busses/i2c-tegra.c
+++ drivers/i2c/busses/i2c-tegra.c
@@ -174,8 +174,6 @@ struct tegra_i2c_dev {
 	unsigned long last_bus_clk_rate;
 	u16 slave_addr;
 	bool is_clkon_always;
-	bool is_high_speed_enable;
-	u16 hs_master_code;
 	int (*arb_recovery)(int scl_gpio, int sda_gpio);
 	struct tegra_i2c_bus busses[TEGRA_I2C_MAX_BUS];
 };
@@ -443,7 +441,6 @@ static int tegra_i2c_init(struct tegra_i2c_dev *i2c_dev)
 	i2c_writel(i2c_dev, val, I2C_CNFG);
 	i2c_writel(i2c_dev, 0, I2C_INT_MASK);
 	clk_set_rate(i2c_dev->clk, i2c_dev->last_bus_clk_rate * 8);
-	i2c_writel(i2c_dev, 0x3, I2C_CLK_DIVISOR);
 
 	val = 7 << I2C_FIFO_CONTROL_TX_TRIG_SHIFT |
 		0 << I2C_FIFO_CONTROL_RX_TRIG_SHIFT;
@@ -638,10 +635,6 @@ static int tegra_i2c_xfer_msg(struct tegra_i2c_bus *i2c_bus,
 		i2c_dev->io_header |= I2C_HEADER_CONT_ON_NAK;
 	if (msg->flags & I2C_M_RD)
 		i2c_dev->io_header |= I2C_HEADER_READ;
-	if (i2c_dev->is_high_speed_enable) {
-		i2c_dev->io_header |= I2C_HEADER_HIGHSPEED_MODE;
-		i2c_dev->io_header |= ((i2c_dev->hs_master_code & 0x7) <<  I2C_HEADER_MASTER_ADDR_SHIFT);
-	}
 	i2c_writel(i2c_dev, i2c_dev->io_header, I2C_TX_FIFO);
 
 	if (!(msg->flags & I2C_M_RD))
@@ -761,8 +754,6 @@ static int tegra_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[],
 
 	rt_mutex_unlock(&i2c_dev->dev_lock);
 
-	i2c_dev->is_high_speed_enable = plat->is_high_speed_enable;
-	i2c_dev->last_bus_clk_rate = plat->bus_clk_rate[0] ?: 100000;
 	i2c_dev->msgs = NULL;
 	i2c_dev->msgs_num = 0;
 
@@ -858,7 +849,6 @@ static int tegra_i2c_probe(struct platform_device *pdev)
 	spin_lock_init(&i2c_dev->fifo_lock);
 
 	i2c_dev->slave_addr = plat->slave_addr;
-	i2c_dev->hs_master_code = plat->hs_master_code;
 	i2c_dev->is_dvc = plat->is_dvc;
 	i2c_dev->arb_recovery = plat->arb_recovery;
 	init_completion(&i2c_dev->msg_complete);
