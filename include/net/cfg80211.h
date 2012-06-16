fa9b4c605d32726db55039e3d5d9d33e7bba699b
Angelo G. Del Regno <kholk11@gmail.com>
2012-06-16 22:44:53 +0200

    {nl,cfg,mac}80211: Allow Setting Multicast Rate in Mesh

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -809,6 +809,7 @@ struct mesh_config {
  * @ie_len: length of vendor information elements
  * @is_authenticated: this mesh requires authentication
  * @is_secure: this mesh uses security
+ * @mcast_rate: multicat rate for Mesh Node [6Mbps is the default for 802.11a]
  *
  * These parameters are fixed when the mesh is created.
  */
@@ -821,6 +822,7 @@ struct mesh_setup {
 	u8 ie_len;
 	bool is_authenticated;
 	bool is_secure;
+	int mcast_rate[IEEE80211_NUM_BANDS];
 };
 
 /**
