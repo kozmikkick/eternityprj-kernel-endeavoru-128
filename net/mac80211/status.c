cfe041b06b9e1d2d3629e3e40e43a8e91fc0a584
Angelo G. Del Regno <kholk11@gmail.com>
2012-06-17 16:58:22 +0200

    net/mac80211/status.c: Fix dangerous warning

--- net/mac80211/status.c
+++ net/mac80211/status.c
@@ -171,7 +171,7 @@ static void ieee80211_check_pending_bar(struct sta_info *sta, u8 *addr, u8 tid)
 		return;
 
 	tid_tx->bar_pending = false;
-	ieee80211_send_bar(sta->sdata, addr, tid, tid_tx->failed_bar_ssn);
+	ieee80211_send_bar(&sta->sdata->vif, addr, tid, tid_tx->failed_bar_ssn);
 }
 
 static void ieee80211_frame_acked(struct sta_info *sta, struct sk_buff *skb)
