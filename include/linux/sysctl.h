2da75d4b6780a72e6e487c7c72dfca21bed16b3f
Angelo G. Del Regno <kholk11@gmail.com>
2012-06-29 21:57:57 +0200

    sysctl: Remove the unnecessary sysctl_set parent concept.

--- include/linux/sysctl.h
+++ include/linux/sysctl.h
@@ -1049,7 +1049,6 @@ struct ctl_table_header
 
 struct ctl_table_set {
 	struct list_head list;
-	struct ctl_table_set *parent;
 	int (*is_seen)(struct ctl_table_set *);
 };
 
@@ -1070,7 +1069,6 @@ struct ctl_path {
 #ifdef CONFIG_SYSCTL
 
 extern void setup_sysctl_set(struct ctl_table_set *p,
-	struct ctl_table_set *parent,
 	int (*is_seen)(struct ctl_table_set *));
 extern void retire_sysctl_set(struct ctl_table_set *set);
 
@@ -1102,7 +1100,6 @@ static inline void unregister_sysctl_table(struct ctl_table_header * table)
 }
 
 static inline void setup_sysctl_set(struct ctl_table_set *p,
-	struct ctl_table_set *parent,
 	int (*is_seen)(struct ctl_table_set *))
 {
 }
