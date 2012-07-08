1ce7e9dc64ac485000e12c276adaf8ff6f984df9
Angelo G. Del Regno <kholk11@gmail.com>
2012-07-08 11:16:57 +0200

    arm: thermal: Removed nonTEGRA_THERMAL_SYSFS logic

--- arch/arm/mach-tegra/board-endeavoru.c
+++ arch/arm/mach-tegra/board-endeavoru.c
@@ -114,19 +114,17 @@ int set_two_phase_freq(int cpufreq);
 
 /* All units are in millicelsius */
 static struct tegra_thermal_data thermal_data = {
-	.temp_throttle = 85000,
 	.temp_shutdown = 90000,
 	.temp_offset = TDIODE_OFFSET, /* temps based on tdiode */
 #ifdef CONFIG_TEGRA_EDP_LIMITS
 	.edp_offset = TDIODE_OFFSET,  /* edp based on tdiode */
 	.hysteresis_edp = 3000,
 #endif
-#ifdef CONFIG_TEGRA_THERMAL_SYSFS
+#ifdef CONFIG_TEGRA_THERMAL_THROTTLE
+	.temp_throttle = 85000,
 	.tc1 = 2,
 	.tc2 = 1,
 	.passive_delay = 500,
-#else
-	.hysteresis_throttle = 1000,
 #endif
 };
 
