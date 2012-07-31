1c6d461a98bc7f1b651771819e0c5012314e80b4
Angelo G. Del Regno <kholk11@gmail.com>
2012-07-31 14:30:09 +0200

    writeback: trace global_dirty_state

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -434,6 +434,7 @@ void global_dirty_limits(unsigned long *pbackground, unsigned long *pdirty)
 	}
 	*pbackground = background;
 	*pdirty = dirty;
+	trace_global_dirty_state(background, dirty);
 }
 
 /**
