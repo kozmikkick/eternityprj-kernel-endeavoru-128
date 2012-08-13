5715d11f136ca404a0b1203434d39ee9e7649f32
Angelo G. Del Regno <kholk11@gmail.com>
2012-08-13 12:45:32 +0200

    treewide: remove useless NORET_TYPE macro and uses

--- arch/mips/include/asm/ptrace.h
+++ arch/mips/include/asm/ptrace.h
@@ -143,7 +143,7 @@ extern int ptrace_set_watch_regs(struct task_struct *child,
 
 extern asmlinkage void do_syscall_trace(struct pt_regs *regs, int entryexit);
 
-extern NORET_TYPE void die(const char *, struct pt_regs *) ATTRIB_NORET;
+extern void die(const char *, struct pt_regs *) ATTRIB_NORET;
 
 static inline void die_if_kernel(const char *str, struct pt_regs *regs)
 {
