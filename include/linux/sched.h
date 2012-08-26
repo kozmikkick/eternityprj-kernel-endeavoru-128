2c31ffddf7cf5447f8ce7c67e4e4dbbc8a00065b
Angelo G. Del Regno <kholk11@gmail.com>
2012-08-26 15:57:00 +0200

    sched: Delay task_contributes_to_load()

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1304,6 +1304,7 @@ struct task_struct {
 
 	/* Revert to default priority/policy when forking */
 	unsigned sched_reset_on_fork:1;
+	unsigned sched_contributes_to_load:1;
 
 #ifdef CONFIG_GENERIC_HARDIRQS
 	/* IRQ handler threads */
