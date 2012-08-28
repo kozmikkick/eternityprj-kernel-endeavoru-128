a4c6b9438a30c55b006398f139243f2ae5800ab8
Angelo G. Del Regno <kholk11@gmail.com>
2012-08-28 15:21:08 +0200

    proc: Linux 3.4

--- fs/proc/namespaces.c
+++ fs/proc/namespaces.c
@@ -24,9 +24,6 @@ static const struct proc_ns_operations *ns_entries[] = {
 #ifdef CONFIG_IPC_NS
 	&ipcns_operations,
 #endif
-#ifdef CONFIG_UTS_NS
-	&utsns_operations,
-#endif
 };
 
 static const struct file_operations ns_file_operations = {
