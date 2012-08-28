69db5f602e8a1338440865e96a6e6ec878fd3957
Angelo G. Del Regno <kholk11@gmail.com>
2012-08-28 13:52:12 +0200

    mm: search from free_area_cache for the bigger size

--- arch/x86/kernel/sys_x86_64.c
+++ arch/x86/kernel/sys_x86_64.c
@@ -128,7 +128,7 @@ arch_get_unmapped_area_topdown(struct file *filp, const unsigned long addr0,
 {
 	struct vm_area_struct *vma;
 	struct mm_struct *mm = current->mm;
-	unsigned long addr = addr0;
+	unsigned long addr = addr0, start_addr;
 
 	/* requested length too big for entire address space */
 	if (len > TASK_SIZE)
@@ -190,6 +190,17 @@ arch_get_unmapped_area_topdown(struct file *filp, const unsigned long addr0,
 		/* try just below the current vma->vm_start */
 		addr = vma->vm_start-len;
 	} while (len < vma->vm_start);
+
+fail:
+	/*
+	 * if hint left us with no space for the requested
+	 * mapping then try again:
+	 */
+	if (start_addr != mm->mmap_base) {
+		mm->free_area_cache = mm->mmap_base;
+		mm->cached_hole_size = 0;
+		goto try_again;
+	}
 
 bottomup:
 	/*
