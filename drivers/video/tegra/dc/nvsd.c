2cb2788859941430e43d2557ca18fff0ca7d83cc
Alexander Vostres <alexx999@gmail.com>
2012-08-30 19:02:33 +0300

    Fixed section mismatches in GPU driver

--- drivers/video/tegra/dc/nvsd.c
+++ drivers/video/tegra/dc/nvsd.c
@@ -898,7 +898,7 @@ int nvsd_create_sysfs(struct device *dev)
 }
 
 /* Sysfs destructor */
-void __devexit nvsd_remove_sysfs(struct device *dev)
+void nvsd_remove_sysfs(struct device *dev)
 {
 	if (nvsd_kobj) {
 		sysfs_remove_group(nvsd_kobj, &nvsd_attr_group);
