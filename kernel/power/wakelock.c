995aaadaa52829e7c6c4b52babdc4ad000e3a3db
Angelo G. Del Regno <kholk11@gmail.com>
2012-09-02 00:12:43 +0200

    PM: wakelocks: Display wakelocks preventing suspend by default
    Use DEBUG_WAKEUP flag to show wakelocks that abort suspend, in
    addition to showing wakelocks held during system resume.
    DEBUG_WAKEUP is enabled by default.

--- kernel/power/wakelock.c
+++ kernel/power/wakelock.c
@@ -252,7 +252,7 @@ long has_wake_lock(int type)
 	unsigned long irqflags;
 	spin_lock_irqsave(&list_lock, irqflags);
 	ret = has_wake_lock_locked(type);
-	if (ret && (debug_mask & DEBUG_SUSPEND) && type == WAKE_LOCK_SUSPEND)
+	if (ret && (debug_mask & DEBUG_WAKEUP) && type == WAKE_LOCK_SUSPEND)
 		print_active_locks(type);
 	else if (ret)
 		print_active_locks(type);
